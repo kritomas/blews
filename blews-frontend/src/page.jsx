932eff02537a6f47bfa279ff578f71f5d13442a5
kritomas <Unknown>
2025-02-24 21:48:36 +0100

    Added page switcher.

--- blews-frontend/src/page.jsx
+++ blews-frontend/src/page.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 
 function Page() {
-	let pageIndex = 0;
 	let data;
 
 	const [pageView, setPageView] = useState(<div/>);
+	const [pageIndex, setPageIndex] = useState(0);
 
-	async function updatePage()
+	async function updatePage(index)
 	{
-		let response = await fetch("/api/page/" + pageIndex);
+		let response = await fetch("/api/page/" + index);
 		data = await response.json();
 		console.log(data);
 		const articles = data.map((val) =>
@@ -24,13 +24,34 @@ function Page() {
 		setPageView(result);
 	}
 
+	function nextPage()
+	{
+		let index = pageIndex + 1
+		setPageIndex(index);
+		setPageView(<div>Loading...</div>)
+		updatePage(index);
+	}
+	function previousPage()
+	{
+		let index = pageIndex - 1
+		setPageIndex(index);
+		setPageView(<div>Loading...</div>)
+		updatePage(index);
+	}
+
 	useEffect(() =>
 	{
-		updatePage();
+		updatePage(pageIndex);
 	}, []);
 
 	return (
 		<div>
+			<div>
+				<p><b>Current Page: </b>{pageIndex}</p>
+				<button onClick={previousPage}>Previous</button>
+				<button onClick={nextPage}>Next</button>
+			</div>
+			<br/>
 			{pageView}
 		</div>
 	)
