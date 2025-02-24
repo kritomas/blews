57bbb3bcadb8021a1eb9cd643d31a24bc058e6d3
kritomas <Unknown>
2025-02-24 20:53:06 +0100

    Added page endpoint.

--- blews-backend/dbconnection.js
+++ blews-backend/dbconnection.js
@@ -2,5 +2,14 @@ import { MongoClient } from "mongodb";
 import dotenv from 'dotenv';
 dotenv.config()
 
-export const client = new MongoClient(process.env.URI)
+const client = new MongoClient(process.env.URI)
 await client.connect();
+
+const collection = client.db("webnews").collection("idnes");
+
+const PAGE_SIZE = 10
+
+export async function getPage(page)
+{
+	return collection.find().skip(PAGE_SIZE * page).limit(PAGE_SIZE).toArray();
+}
--- blews-backend/main.js
+++ blews-backend/main.js
@@ -1,11 +1,26 @@
 import express from "express";
 import cors from "cors";
-import {client} from "./dbconnection.js"
+import {getPage} from "./dbconnection.js"
 
 const PORT = 42069;
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get("/api/page/:page", async (req, res, next) =>
+{
+	try
+	{
+		const page = req.params.page;
+		const pages = await getPage(page);
+		if (pages === undefined) res.status(404).send("Not found");
+		else res.status(200).send(pages);
+	}
+	catch (e)
+	{
+		next(e);
+	}
+});
 
 app.listen(PORT, () =>
 {
