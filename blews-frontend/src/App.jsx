8d6cfff0e6ab36addbf40b7656adab21586f8903
kritomas <Unknown>
2025-02-24 21:02:52 +0100

    Initial frontend prep.

---
+++ blews-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Page from "./page.jsx"
+
+function App() {
+	return (
+		<div>
+			<Page/>
+		</div>
+	)
+}
+
+export default App;
---
+++ blews-frontend/src/page.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function Page() {
+	const [currentPage, setCurrentPage] = useState(0);
+
+	return (
+		<div>
+			Pag
+		</div>
+	)
+}
+
+export default Page;
